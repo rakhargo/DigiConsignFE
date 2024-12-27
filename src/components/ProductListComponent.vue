a30dff95aa62467b480d825fa463d49c1b931782
rakhargo <rakhargo@gmail.com>
2024-12-27 16:15:38 +0700

    add loading indicator di home

--- src/components/ProductListComponent.vue
+++ src/components/ProductListComponent.vue
@@ -1,60 +1,36 @@
 <!-- ProductList.vue -->
 <script setup lang="ts">
 import ProductCard from './ProductCardComponent.vue';
-
-// const imageUrls = [""];
-
-// const products = ref([
-//   {
-//     id: '1',
-//     name: 'Vortexseries Oni R1',
-//     location: 'Kota Bekasi',
-//     price: 100000,
-//     condition: 'Judge By Pict',
-//     imageUrl: '/images/product/product1.jpeg',
-//   },
-//   {
-//     id: '2',
-//     name: 'Rexus Daxa Asteria V2',
-//     location: 'Kota Bandung',
-//     price: 250000,
-//     condition: 'Very Good Condition',
-//     imageUrl: '/images/product/product2.jpeg',
-//   },
-//   {
-//     id: '3',
-//     name: 'Logitech M191',
-//     location: 'Kab. Bandung',
-//     price: 50000,
-//     condition: 'Good Condition',
-//     imageUrl: '/images/product/product3.jpeg',
-//   },
-// ]);
-
+import LoadingIndicator from './LoadingIndicatorComponent.vue';
 import { ref, onMounted } from 'vue';
 
-    //import api
-    import api from '../api';
+import api from '../api';
 
-    //define state
-    const products = ref([]);
+const products = ref([]);
+const loading = ref(true);
 
-    async function fetchAllProduct() {
-      //fetch data 
-      const response = await api.get('/api/product');
-      products.value = response.data;
-      console.log(products.value);
-      
-    }
+async function fetchAllProduct() {
+  loading.value = true;
 
-    onMounted(() => {
-      fetchAllProduct();
-    });
+  try {
+    const response = await api.get('/api/product');
+    products.value = response.data;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    loading.value = false;
+  }
+}
+
+onMounted(() => {
+  fetchAllProduct();
+});
 
 </script>
 
 <template>
   <!-- <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4"> -->
+  <LoadingIndicator v-if="loading"/>
   <div class="product-list flex flex-wrap justify-start gap-4 p-4">
     <ProductCard 
       v-for="(product, index) in products" 
